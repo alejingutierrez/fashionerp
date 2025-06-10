5fe11a5d114d6488de21a62f7362831d675fd51c
alejingutierrez <malgutierrezar@gmail.com>
2025-06-10 05:58:30 -0500

    feat(ui): enhance PrimaryButton

---
+++ frontend/src/components/atoms/PrimaryButton.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SaveIcon from '@mui/icons-material/Save';
+import { PrimaryButton } from './PrimaryButton';
+
+const meta: Meta<typeof PrimaryButton> = {
+  title: 'Atoms/PrimaryButton',
+  component: PrimaryButton,
+  args: {
+    children: 'Save',
+  },
+  argTypes: {
+    onClick: { action: 'clicked' },
+    startIcon: { control: false },
+    endIcon: { control: false },
+    loadingPosition: {
+      control: 'select',
+      options: ['center', 'start', 'end'],
+    },
+    loadingText: { control: 'text' },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof PrimaryButton>;
+
+export const Default: Story = {};
+
+export const Disabled: Story = {
+  args: { disabled: true, children: "Can't click" },
+};
+
+export const WithIcon: Story = {
+  args: { startIcon: <SaveIcon /> },
+};
+
+export const Loading: Story = {
+  args: { loading: true },
+};
+
+export const LoadingStart: Story = {
+  args: { loading: true, loadingPosition: 'start', children: 'Save' },
+};
+
+export const LoadingWithText: Story = {
+  args: { loading: true, loadingText: 'Saving...' },
+};
