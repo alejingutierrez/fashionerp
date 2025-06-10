42621c26d85ed3cf24afe80b5afdc164fb454759
alejingutierrez <malgutierrezar@gmail.com>
2025-06-10 06:43:50 -0500

    Revert "Add SecondaryButton atom"

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  moduleDirectories: ['node_modules', 'src'],
-};
